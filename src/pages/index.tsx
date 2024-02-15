4c23c3c8423d6cd22c46ab32abac9f4734262f12
ueni.tech <ueni.tech@gmail.com>
2024-02-15 21:30:09 +0900

    #1 update: 不要な記述の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Button } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Chakra UI</title>
+      </Head>
       <Button colorScheme="teal" size="md">
         Chakra UI ボタン
       </Button>
