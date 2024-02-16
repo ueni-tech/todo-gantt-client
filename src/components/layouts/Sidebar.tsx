0056d2da6166806bcea32e324e76c4d563ff1f2a
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-02-16 18:08:14 +0900

    #1 update: Sidebar Component

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,18 +1,18 @@
 import { AddIcon } from '@chakra-ui/icons'
-import { Box, Image, Stack } from '@chakra-ui/react'
+import { Box, Button, Image, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 const Sidebar = () => {
   return (
     <Box bgColor="main" p={4} h="calc(100vh - 56px)">
       <Stack spacing={5}>
-        <Box as="button" w='40px' h='40px' border="2px solid #ccc" bgColor="gray.200" borderRadius="md">
-          <Image src="/images/team_icons/team_01.jpg" alt="team_01" />
+        <Box as='button' w='40px' h='40px' border="2px solid #ccc" bgColor="gray.200" borderRadius="md" _hover={{opacity: '0.7'}}>
+          <Image src="/images/team_icons/team_01.jpg" alt="team_01" w='100%'  />
         </Box>
-        <Box as="button" w='40px' h='40px' border="2px solid #ccc" bgColor="gray.200" borderRadius="md">
-          <Image src="/images/team_icons/team_02.webp" alt="team_02" />
+        <Box as='button' w='40px' h='40px' border="2px solid #ccc" bgColor="gray.200" borderRadius="md" _hover={{opacity: '0.7'}}>
+          <Image src="/images/team_icons/team_02.webp" alt="team_02" w='100%'  />
         </Box>
-        <Box as="button" w='40px' h='40px' bgColor="gray.500" borderRadius="md" display='flex' justifyContent='center' alignItems='center'>
+        <Box as="button" w='40px' h='40px' bgColor="gray.500" borderRadius="md" display='flex' justifyContent='center' alignItems='center' _hover={{ opacity: '0.7' }}>
           <AddIcon color="white" />
         </Box>
       </Stack>
