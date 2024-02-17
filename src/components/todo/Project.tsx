23372bfd9fce533cc45d8e39176b169eb3d166ea
ueni.tech <ueni.tech@gmail.com>
2024-02-17 17:35:44 +0900

    #1 update: Project, Todo

--- src/components/todo/Project.tsx
+++ src/components/todo/Project.tsx
@@ -1,14 +1,31 @@
-import { Box, Heading, Stack } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Stack } from '@chakra-ui/react'
 import React from 'react'
 import Todo from './Todo'
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons'
 
 const Project = () => {
   return (
     <Box p={4} bgColor='blackAlpha.300' borderRadius='md' h='30vh'>
-      <Heading fontSize='xs' mb={3}>プロジェクト名</Heading>
+      <Flex justify='space-between' align='center' mb={3}>
+        <Heading fontSize='xs'>プロジェクト名</Heading>
+        <Popover>
+          <PopoverTrigger>
+            <DeleteIcon color='blackAlpha.200' cursor='pointer' _hover={{ color: 'blackAlpha.500' }} />
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>このプロジェクトを削除しますか？</PopoverHeader>
+            <PopoverBody>
+              <Button colorScheme='red' variant='outline' size='sm'>削除</Button>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Flex>
       <Stack spacing={3}>
         <Todo />
         <Todo />
+        <IconButton size='sm' colorScheme='blackAlpha' w='20px' aria-label="add todo" icon={<AddIcon />} />
       </Stack>
     </Box>
   )
--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,12 +1,30 @@
-import { Box, Button, Text } from '@chakra-ui/react'
+import { DeleteIcon } from '@chakra-ui/icons'
+import { Box, Button, Checkbox, Flex, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Text } from '@chakra-ui/react'
+import { color } from 'framer-motion'
 import React from 'react'
 
 const Todo = () => {
   return (
     <Box>
-      <Box p={3} bgColor='orange.50' borderRadius='md'>
-        <Text fontSize='xs'>タスク</Text>
-      </Box>
+      <Flex p={3} bgColor='orange.50' borderRadius='md' justify='space-between'>
+        <Flex justify='center' align='center'>
+          <Checkbox mr={2} />
+          <Text fontSize='xs' noOfLines={1} >タスク</Text>
+        </Flex>
+        <Popover>
+          <PopoverTrigger>
+            <DeleteIcon color='red.200' cursor='pointer' _hover={{ color: 'red.500' }} />
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>このタスクを削除しますか？</PopoverHeader>
+            <PopoverBody>
+              <Button colorScheme='red' variant='outline' size='sm'>削除</Button>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Flex>
     </Box>
   )
 }
